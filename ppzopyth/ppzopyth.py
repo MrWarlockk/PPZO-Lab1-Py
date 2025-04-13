77fad30f0b2bb12ac966f7d35f8d69631876f9e8
MrWarlockk <gnatmaciej@gmail.com>
2025-04-13 23:05:10 +0200

    Zadanie 2

--- ppzopyth/ppzopyth.py
+++ ppzopyth/ppzopyth.py
@@ -39,8 +39,37 @@ def calcFunc():
             return None;
         return numb1/numb2;
 
+
+def temperFunc():
+
+    while True:
+        print("Podaj jednostke wejsciowa: ")
+        print("C: Zamiana stopni Celciusza na Fahrenheit ")
+        print("F: Zamiana stopni Fahrenheit na Celciusza ")
+
+        conversionChoice = input("Wybor: ")
+        if(conversionChoice == "c" or conversionChoice == "C" or conversionChoice == "f" or conversionChoice == "F"):
+            while True:
+                try:
+                    degreeValue = float(input("Podaj ilosc stopni: "))
+                    break;
+                except ValueError:
+                    print("Nieprawidlowa wartosc, podaj liczbe.")
+
+            if(conversionChoice == "c" or conversionChoice == "C"):
+                return ((degreeValue * 1.8) + 32);
+                
+            elif(conversionChoice == "f" or conversionChoice == "F"):
+                return ((degreeValue - 32) / 1.8);
+
+            else:
+                print("Nieprawidlowy input")
+                return None;
+        else:
+            print("Nieprawidlowa jednostka, podaj poprawna.")
+
 while True:
-    choiceMain = input("Input(1: Zadanie 1, pozostale symbole wyjscie z konsoli): ")
+    choiceMain = input("Input(1: Zadanie 1, 2: Zadanie 2, 3: Zadanie 3, pozostale symbole wyjscie z konsoli): ")
     if(choiceMain == "1"):
 
         resultCalc = calcFunc()
@@ -49,6 +78,14 @@ while True:
             print(f"Wynik to: {resultCalc}")
         else:
             print("Nie mozna dzielic przez 0")
+
+    elif(choiceMain == "2"):
+
+        temperFuncRet = temperFunc()
+
+        if (temperFuncRet != None):
+            print(f"Wynik to: {temperFuncRet}")
+        else: print("Wystapil blad")
     else:
         break;
 
